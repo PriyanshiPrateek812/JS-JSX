85d9d56cd3489600c1e6e29351cb6ccbe4f80afb
PriyanshiPrateek812 <154115675+PriyanshiPrateek812@users.noreply.github.com>
2025-02-21 09:33:11 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import Test from "./components/Test";
+import User from "./components/User";
+
+function App() {
+
+  return (
+    <div className="container">
+        {/* <User /> */}
+        <Test />
+    </div>
+  );
+}
+
+export default App;
